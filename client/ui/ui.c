ae77c678b04f12abda6bf9a2dc0ff414dece5f0f
Axel Isaksson <isaksson.axel@telia.com>
2012-10-27 10:53:47 +0200

    even fancier makefiles

--- client/ui/ui.c
+++ client/ui/ui.c
@@ -1,8 +1,6 @@
-#include <string.h>
-
 #include "ui.h"
 
 void ui_init() {
 	ui_selected_widget=NULL;
-	memset(&ui_e_m_prev, 0, sizeof(ui_e_m_prev));
+	ui_e_m_prev.x=ui_e_m_prev.y=ui_e_m_prev.buttons=ui_e_m_prev.wheel=0;
 }
