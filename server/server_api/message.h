b28f0aa0771def5bf4d47139431ee31ae76ce3f5
Steven Arnow <s@rdw.se>
2012-10-28 18:18:37 +0100

    Added the rest of the commands

--- server/server_api/message.h
+++ server/server_api/message.h
@@ -3,6 +3,13 @@
 
 #define	MSG_PAYLOAD_BIT			0x100
 
+#define	MSG_TILE_ATTRIB_POWER		0x1
+#define	MSG_TILE_ATTRIB_FOW		0x2
+
+#define	MSG_BUILDING_STOP		0
+#define	MSG_BUILDING_START		1
+#define	MSG_BUILDING_HOLD		2
+
 /* Message protocol, general stuff */
 
 /* This struct is used for *all* message bodies. Ints are in correct (big) endian */
@@ -64,8 +71,29 @@ typedef struct {
 /* No arguments. No data. When you get this message, you should switch from lobby to game play mode */
 #define	MSG_RECV_GAME_START		0x00A
 
+/* arg_1 is building type, arg_2 is index on the map (x + y * map_w). */
+#define	MSG_RECV_BUILDING_PLACE		0x00B
+
+/* arg_1 is index on the map that the building is at, arg_2 is the shield value */
+#define	MSG_RECV_BUILDING_SHIELD	0x00C
+
+/* arg_1 is index on the map that the building is at, arg_2 is the HP value */
+#define	MSG_RECV_BUILDING_HP		0x00D
+
+/* arg_1 is the index on the map that the building is at, arg_2 is the index of the building attacking */
+#define	MSG_RECV_BUILDING_ATTACK	0x00E
+
+/* arg_1 is the building type that is being built, arg_2 is the progress in the range 0..1000 */
+#define	MSG_RECV_BUILDING_PROGRESS	0x00F
+
+/* arg_1 is tile index on the map, arg_2 is the attribute bitmask for that tile */
+#define	MSG_RECV_MAP_TILE_ATTRIB	0x010
+
+/* No arguemnts. No data. */
+#define	MSG_RECV_PLAYER_DEFEATED	0x011
 
 
+/* These commands actually have data after them */
 
 /* player_id is always 0. arg_1 is the size of the map in bytes. The message has data (map file name.) */
 #define	MSG_RECV_MAP_BEGIN		0x100
@@ -99,6 +127,14 @@ typedef struct {
 /* arg_1 is 1 if client is ready, 0 if not. No data. */
 #define	MSG_SEND_READY			4
 
+/* arg_1 is the building type to build. arg_2 is the action (0 = stop, 1 = start, 2 = hold) */
+#define	MSG_SEND_START_BUILD		5
+
+/* arg_1 is the building type to place, arg_2 is the index on the map to place it on */
+#define	MSG_SEND_PLACE_BUILDING		6
+
+/* arg_1 is map index the building to set attack from is at, arg_2 the tile index for the target building */
+#define	MSG_SEND_SET_ATTACK		7
 
 
 #endif
