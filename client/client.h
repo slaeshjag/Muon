aa1888658871ce124b145f20be6180b75810647a
Axel Isaksson <isaksson.axel@telia.com>
2012-10-28 15:03:25 +0100

    started work on networking

--- client/client.h
+++ client/client.h
@@ -1,8 +1,16 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#include "../server/server_api/server.h"
+
 DARNIT_SOCKET *sock;
+MESSAGE_RAW msg_recv;
+void *msg_recv_offset;
+int player_id;
+
+void (*client_message_handler)(MESSAGE_RAW *);
 
 int client_init(char *host, int port);
+void client_check_incomming();
 
 #endif
