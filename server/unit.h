eb11e328499a4a3a87e466c2ef2f59939a8cff67
kqr <k@rdw.se>
2012-11-04 22:38:36 +0100

    tweaked the units, added array for damage

--- server/unit.h
+++ server/unit.h
@@ -17,13 +17,83 @@
 #define	UNIT_ATTACKER_DMGP		5
 #define	UNIT_REGEN_DELAY		5000
 
-static const int unit_los[] = 		{ 0, 7, 6, 3, 4, 1, 2};
-static const int unit_maxhp[] = 	{ 0, 5000000, 100000, 100000, 500000, 100000, 100000 };
-static const int unit_maxshield[] = 	{ 0, 5000000, 1, 500000, 500000, 4000000, 100000 };		/* Shield must not be 0 for any existing building! */
-static const int unit_shieldreg[] = 	{ 0, 5, 5, 5, 5, 5, 5 };
+static const int unit_los[] = 		{ 0,          /* nothing */
+	                                  7,          /* generator */
+	                                  6,          /* scout */
+	                                  3,          /* attacker */
+	                                  4,          /* pylon */
+	                                  1,          /* wall */
+	                                  7           /* owned control point (buildsite) */
+	                                };
+
+/* 750 is the base coefficient for hp,
+ * to make numbers easier to work with.
+ * think of the non-multiplied number as
+ * how many tenths of a second it takes
+ * to kill a building with a given number
+ * of attacking units, when the building
+ * is unpowered. */
+static const int unit_maxhp[] = 	{ 750 * 0,       /* nothing */
+	                                  750 * 150,     /* generator */
+	                                  750 * 50,      /* scout */
+	                                  750 * 90,      /* attacker */
+	                                  750 * 40,      /* pylon */
+	                                  750 * 10,      /* wall */
+	                                  750 * 50       /* owned control point (buildsite) */
+	                                };
+
+/* Shield must not be 0 for any existing building! */
+/* same coefficient as hp, the exception being
+ * buildings that "doesn't have" shields. */
+static const int unit_maxshield[] =  { 0,              /* nothing */
+	                                   750 * 150,    /* generator */
+	                                   1,              /* scout */
+	                                   750 * 60,     /* attacker */
+	                                   750 * 30,     /* pylon */
+	                                   750 * 90,     /* wall */
+	                                   1               /* owned control point (buildsite) */
+	                                 };
+
+static const int unit_shieldreg[] =  { 0,          /* nothing */
+	                                   1,          /* generator */
+	                                   1,          /* scout */
+	                                   1,          /* attacker */
+	                                   1,          /* pylon */
+	                                   1,          /* wall */
+	                                   1           /* owned control point (buildsite) */
+	                                 };
+
+/* Meant to replace UNIT_ATTACKER_DMGP */
+static const int unit_damage[] =     { 0,          /* nothing */
+	                                   0,          /* generator */
+	                                   1,          /* scout */
+	                                   5,          /* attacker */
+	                                   0,          /* pylon */
+	                                   0,          /* wall */
+	                                   0           /* owned control point (buildsite) */
+	                                 };
+
 /* Pylons and generators must have the same distribution range... */
-static const int unit_range[] = 	{ 0, 4, 0, 3, 4, 1, 1 };
-static const int unit_buildtime[] =	{ 0, 0, 5000, 20000, 20000, 10000, 10000 };
+/* range is for shooting and power distribution, whereever
+ * applicable. units that don't do either have a range of 0. */
+static const int unit_range[] = 	 { 0,          /* nothing */
+                                       4,          /* generator, distribution range */
+                                       4,          /* scout, attack range */
+                                       5,          /* attacker, attack range */
+                                       4,          /* pylon, redistribution range */
+                                       0,          /* wall */
+                                       0           /* owned control point (buildsite) */
+                                     };
+
+/* measured in ticks, of which there are 3000/s with normal game speed. */
+static const int unit_buildtime[] =	 { 0,          /* nothing */
+	                                   0,          /* generator */
+	                                   5000,       /* scout */
+	                                   20000,      /* attacker */
+	                                   10000,      /* pylon */
+	                                   10000,      /* wall */
+	                                   10000       /* owned control point (buildsite) */
+	                                 };
 
 struct SERVER_UNIT;
 
