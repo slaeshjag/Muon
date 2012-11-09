a900d4d925d7c4d1a28880c0c3ec23f556313b99
h4xxel <isaksson.axel@telia.com>
2012-11-09 21:22:21 +0100

    added license headers to all files

--- client/ui/vbox.c
+++ client/ui/vbox.c
@@ -1,3 +1,22 @@
+/* Muon - a new kind of rts
+ * Copyright 2012 Steven Arnow <s@rdw.se> and Axel Isaksson (mr3d/h4xxel)
+ * 
+ * This file is part of Muon.
+ * 
+ * Muon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Muon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Muon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "ui.h"
 
 UI_WIDGET *ui_widget_create_vbox() {
