e641de87142207da8f82fc0626156cdcabe91b49
h4xxel <isaksson.axel@telia.com>
2013-01-31 14:45:31 +0100

    added missing license headers

--- client/menu/credits.h
+++ client/menu/credits.h
@@ -17,34 +17,9 @@
  * along with Muon.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef GAMEOVER_H
-#define GAMEOVER_H
+#ifndef CREDITS_H
+#define CREDITS_H
 
-struct UI_PANE_LIST panelist_gameover_sidebar;
-struct UI_PANE_LIST panelist_gameover_stats;
 
-UI_WIDGET *gameover_sidebar_button_menu;
-UI_WIDGET *gameover_stats_label_time;
-char gameover_stats_label_time_text[32];
-UI_WIDGET *gameover_stats_hbox;
-UI_WIDGET *gameover_stats_vbox_stat[6];
-
-struct GAMEOVER_STATLABEL {
-	UI_WIDGET *built;
-	char built_text[8];
-	UI_WIDGET *lost;
-	char lost_text[8];
-	UI_WIDGET *destroyed;
-	char destroyed_text[8];
-	UI_WIDGET *efficiency;
-	char efficiency_text[8];
-	UI_WIDGET *score;
-	char score_text[8];
-	UI_WIDGET *name;
-} *gameover_statlabel;
-
-void gameover_init();
-void gameover_update_stats();
-void gameover_sidebar_button_menu_click(UI_WIDGET *widget, unsigned int type, UI_EVENT *e);
 
 #endif
